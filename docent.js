758e7d7aa1accdcf03360cb97d09f97baccb16ad
Sam Eubank <sameubank+zerious@gmail.com>
2014-09-09 09:55:52 -0700

    Dummy traversal of lighter.io

--- docent.js
+++ docent.js
@@ -1,3 +1,23 @@
+#!/usr/bin/env node
+
+// When running directly, start the CLI.
+if (process.mainModule == module) {
+
+  var shellify = require('shellify');
+  shellify({
+    commands: {
+      gen: {
+        note: 'Writes */README.md contents to *.ltl files',
+        options: {
+          //path: 'The path to traverse [.]'
+        }
+      }
+    }
+  });
+
+}
+
+
 var docent = module.exports;
 
 /**
