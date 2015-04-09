af2066f222310209b8c8d33971f8aa17892288de
Sam Eubank <sameubank+zerious@gmail.com>
2015-04-09 09:08:02 -0700

    v1.0.0

--- scripts/docs.js
+++ scripts/docs.js
@@ -1,17 +1,21 @@
-onReady(function () {
-  var nav = getElement('_DOCENT_NAV');
-  var doc = getElement('_DOCENT_DOC');
-  var html = getHtml(nav);
-  if (nav && doc && !html) {
-    var headings = [];
-    html = '<form><input type="text" name="q"></form>';
-    getHtml(doc).replace(/<h(\d) id="(.*?)">/g, function (match, level, id) {
-      var heading = getElement(id);
-      heading._DEPTH = level - 2;
-      headings.push(heading);
-      var text = getText(heading).replace(/\(.*$/, '');
-      html += '<a href="#' + id + '">' + text + '</a>';
-    });
-    setHtml(nav, html);
+// @use jymin/jymin.js
+
+Jymin.onReady(function () {
+  var nav = Jymin.getElement('_docentNav');
+  var doc = Jymin.getElement('_docentDoc');
+  if (nav && doc) {
+    var html = Jymin.getHtml(nav);
+    if (!html) {
+      var headings = [];
+      html = '<t-form><t-field type="search"></t-field></t-form>';
+      Jymin.getHtml(doc).replace(/<h(\d) id="(.*?)">/g, function (match, level, id) {
+        var heading = Jymin.getElement(id);
+        heading._depth = level - 2;
+        headings.push(heading);
+        var text = Jymin.getText(heading).replace(/\(.*$/, '');
+        html += '<a href="#' + id + '">' + text + '</a>';
+      });
+      Jymin.setHtml(nav, html);
+    }
   }
 });
